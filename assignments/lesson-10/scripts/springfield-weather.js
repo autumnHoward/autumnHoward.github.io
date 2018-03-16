0aa23e4d7d2f03bf9fe2855ffd82fbafb38906e4
AutumnHoward <autumn5273@gmail.com>
2018-03-16 01:53:04 -0600

    weather site refinements

---
+++ assignments/lesson-10/scripts/springfield-weather.js
@@ -0,0 +1,18 @@
+var requestURL = 'http://api.wunderground.com/api/c68b3124bacf9ecd/conditions/q/TN/Springfield.json';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL, true);
+request.send();
+
+request.onload = function(){
+    var weatherInfo = JSON.parse(request.responseText)
+    console.log(weatherInfo);
+    
+    document.getElementById('place').innerHTML = weatherInfo.current_observation.display_location.full;
+    document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temperature_string;
+    document.getElementById('feelTemp').innerHTML = weatherInfo.current_observation.feelslike_string;
+    document.getElementById('icon').innerHTML = weatherInfo.current_observation.icon;
+    document.getElementById('weather-icon').src = weatherInfo.current_observation.icon_url;
+    document.getElementById('precip').innerHTML = weatherInfo.current_observation.precip_today_string;
+    document.getElementById('windspeed').innerHTML = weatherInfo.current_observation.wind_mph;
+    document.getElementById('windchill').innerHTML = weatherInfo.current_observation.windchill_string;
+}
